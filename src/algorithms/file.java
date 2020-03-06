8527f873f713158818a568fec71784020fcf54ca
cyoni <cyoni10@gmail.com>
2020-03-06 21:09:22 +0200

    Added Json parsing

--- src/algorithms/file.java
+++ src/algorithms/file.java
@@ -1,58 +1,102 @@
 package algorithms;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
 
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+
 
 /**
+ * 
 	* This class loads/process/saves files
 	*@author Yoni
 	*/
 
 	public class file implements file_interface{
 
-		private JFrame f;
-		private String file_location;
+	private JFrame f;
+	private String file_location;
+
+	public file(JFrame f) {
+		this.f = f;
+	}
+	
+	public file(String l) {
+		setLocation(l);
+	}
 		
-		public file(JFrame f) {
-			this.f = f;
+	@Override
+	public void loadFile() {
+	JFileChooser fileChooser = new JFileChooser();
+	fileChooser.setCurrentDirectory(new File(System.getProperty("user.dir")));
+	int result = fileChooser.showOpenDialog(f);
+		if (result == JFileChooser.APPROVE_OPTION) { // user selects a file
+			File selectedFile = fileChooser.getSelectedFile();
+			System.out.println("Selected file: " + selectedFile.getAbsolutePath());
+			setLocation(selectedFile.getAbsolutePath());
+			} 
 		}
 		
-		public file(String l) {
-			setLocation(l);
-		}
+
+	@Override
+	public void processFile() {
+	if (getFilePath().isEmpty()) return;
 		
-		@Override
-		public void loadFile() {
-			JFileChooser fileChooser = new JFileChooser();
-			fileChooser.setCurrentDirectory(new File(System.getProperty("user.dir")));
-			int result = fileChooser.showOpenDialog(f);
-			if (result == JFileChooser.APPROVE_OPTION) { // user selects a file
-				File selectedFile = fileChooser.getSelectedFile();
-				System.out.println("Selected file: " + selectedFile.getAbsolutePath());
-				setLocation("selectedFile.getAbsolutePath()");
+	String data = "";
+	try {
+		data = new String(Files.readAllBytes(Paths.get(getFilePath())));
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
+	System.out.println(data);
+	
+		
+	try {
+			JSONObject obj = new JSONObject(data);
+			JSONArray arr = obj.getJSONArray("Edges");
+			for (int i = 0; i < arr.length(); i++) // get data of Edges
+				{
+					int src = arr.getJSONObject(i).getInt(("src"));
+					double w = arr.getJSONObject(i).getDouble(("w"));
+					int dest = arr.getJSONObject(i).getInt(("dest"));	 
+				}
+			arr = obj.getJSONArray("Nodes"); // get data of Nodes
+			for (int i = 0; i < arr.length(); i++) // get data of Edges
+			{
+				String pos = arr.getJSONObject(i).getString(("pos"));
+				int id = arr.getJSONObject(i).getInt(("id"));	
+
 			}
-			
 		}
 		
+		 catch (JSONException e) {
+				e.printStackTrace();
+			}
+	}
 
-		@Override
-		public void processFile() {
-
-			
-		}
-
-		@Override
-		public void saveFile() {
+	@Override
+	public void saveFile() {
 
 			
-		}
+	}
 
 		@Override
-		public void setLocation(String l) {
-		this.file_location = l;
-		}
+	public void setLocation(String l) {
+	this.file_location = l;
+	}
+		
+	@Override
+	public String getFilePath() {
+	return file_location;
+	}
 
 		
 		
